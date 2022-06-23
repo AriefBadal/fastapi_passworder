cb071a2f243f5e7d1c207e52277fc661034dadd8
Arief Badal <1042170@hr.nl>
2022-06-23 11:35:27 +0200

    Fix  style according to flake8

--- passworder/logger.py
+++ passworder/logger.py
@@ -1,7 +1,11 @@
 import logging
+
+
 # Passworder logger
 async def password_logger():
     # Configuration for the logger
-    logging.basicConfig(level=logging.INFO, filename="passworder.log", filemode="w",
-    format="%(asctime)s - %(levelname)s - %(message)s")
-    return logging.info(f'There was a new password request')
+    logging.basicConfig(
+                        level=logging.INFO, filename="passworder.log",
+                        filemode="w",
+                        format="%(asctime)s - %(levelname)s - %(message)s")
+    return logging.info('There was a new password request')
